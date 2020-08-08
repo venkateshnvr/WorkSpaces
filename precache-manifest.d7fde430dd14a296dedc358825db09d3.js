dbf2bf1e4a77e0cefb9822a26ed1560c19199f7d
venkateshnvr <nanduri557@gmailcom>
2020-08-09 02:20:06 +0530

    Updates

---
+++ precache-manifest.d7fde430dd14a296dedc358825db09d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57caa7bbdb93ab343d5976b372c66923",
+    "url": "/venkateshnvr/WorkSpaces.git/index.html"
+  },
+  {
+    "revision": "8c95d99b852c6dcdd48e",
+    "url": "/venkateshnvr/WorkSpaces.git/static/css/main.f3d4d599.chunk.css"
+  },
+  {
+    "revision": "1ca050dd961ada241069",
+    "url": "/venkateshnvr/WorkSpaces.git/static/js/2.60a1c6a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/venkateshnvr/WorkSpaces.git/static/js/2.60a1c6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c95d99b852c6dcdd48e",
+    "url": "/venkateshnvr/WorkSpaces.git/static/js/main.1b03ee2b.chunk.js"
+  },
+  {
+    "revision": "a362f122df76c7024567",
+    "url": "/venkateshnvr/WorkSpaces.git/static/js/runtime-main.f8239c66.js"
+  }
+]);
